7ff4f4307e590b306e9e220483904b47435b3ba3
Jack <jaxanimations@gmail.com>
2020-07-01 09:28:29 -0500

    Added functionality to add semseter and remove semester buttons

--- scripts/app-scripts.js
+++ scripts/app-scripts.js
@@ -1,13 +1,51 @@
+/*
+Init routines for app section.
+*/
 function init()
 {
+    // Get blank HTML for semester editor
+    var semesterEditorHTML = document.getElementById("semester-editor-wrapper").innerHTML;
 
+    // Add event listener to add semester button
+    document.getElementById("add-semester-button").addEventListener("click", function()
+    {
+        addSemester(semesterEditorHTML);
+    })
+
+    // Add event listener to each remove semester button
+    var removeSemesterButtons = document.getElementsByClassName("remove-semester-button");
+    for (var count = 0; count < removeSemesterButtons.length; count++)
+    {
+        removeSemesterButtons[count].addEventListener("click", removeSemester);
+    }
 }
 
+/*
+Removes selected semester editor. 
+*/
+function removeSemester()
+{
+    this.parentNode.parentNode.removeChild(this.parentNode);
+}
 
 /*
-This function adds and populates a new empty semester editor.
+This function adds a new empty semester editor.
 */
-function addSemester()
+function addSemester(semesterEditorHTML)
 {
+    // Get div to append new semester editor to
+    var semesterEditorWrapper = document.getElementById("semester-editor-wrapper");
 
+    // Add template HTML to wrapper div
+    semesterEditorWrapper.innerHTML += semesterEditorHTML;
+
+    // Scroll down to newly added semester editor card
+    location.hash = "#" + "semester-editor-button-anchor";
+
+    // Add event listener to each remove semester button
+    var removeSemesterButtons = document.getElementsByClassName("remove-semester-button");
+    for (var count = 0; count < removeSemesterButtons.length; count++)
+    {
+        removeSemesterButtons[count].addEventListener("click", removeSemester);
+    }
 }
